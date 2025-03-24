a318f347ea471d86b17e9c0a4d2b2568899afd71
kamada <kamada@example.com>
2025-03-24 15:09:22 +0900

    DetailsPage コンポーネントを配置

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import DetailsPage from "./pages/DetailsPage";
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <h1 className="my-4 display-5">Vocab App</h1>
       <Routes>
         <Route index element={<HomePage />} />
+        <Route path="/details/:id" element={<DetailsPage />} />
       </Routes>
     </div>
   );
