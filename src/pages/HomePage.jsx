459728e7e185d6d5c8c92abc5f823a5c4a2ddb99
kamada <kamada@example.com>
2025-03-24 11:27:15 +0900

    単語リストの表示ページの作成

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import { getVocabs } from "../api/vocabApi";
+import VocabListTable from "../components/VocabListTable";
+
+/*
+単語リストページ
+単語登録フォーム、単語の種類(絞り込み用)、単語リストを表示
+⇒絞り込み条件に応じて、単語リストを取得する
+*/
+
+export default function HomePage() {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["vocabs"],
+    queryFn: getVocabs
+  });
+
+  // 単語リストテーブル
+  let vocabData;
+  if (isError) {
+    vocabData = <div className="alert alert-danger">エラーが発生しました</div>;
+  } else if (isLoading) {
+    vocabData = <p>Loading...</p>;
+  } else if (data.length === 0) {
+    vocabData = (
+      <div className="alert alert-warning">単語情報が存在しません</div>
+    );
+  } else {
+    vocabData = <VocabListTable vocabList={data} />;
+  }
+  return (
+    <div>
+      {/*単語登録フォームの配置*/}
+      {/*絞り込みメニューの配置*/}
+      {vocabData}
+    </div>
+  );
+}
