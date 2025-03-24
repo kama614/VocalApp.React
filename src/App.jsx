62be63c5fff51482269021af1c7d9ed9083ca67b
kamada <kamada@example.com>
2025-03-24 11:27:30 +0900

    HomePage コンポーネントを配置

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
 export default function App() {
   return (
     <div className="container">
       <h1 className="my-4 display-5">Vocab App</h1>
+      <Routes>
+        <Route index element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
